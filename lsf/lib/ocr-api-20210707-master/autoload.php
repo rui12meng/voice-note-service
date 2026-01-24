8acccdd8ba07133bbae8cd8e33edd6651b165389
mengrui <mengrui@okay.cn>
2026-01-24 21:15:18 +0800

    test

--- lsf/lib/ocr-api-20210707-master/autoload.php
+++ lsf/lib/ocr-api-20210707-master/autoload.php
@@ -3,7 +3,7 @@
 function ocrLoader($class)
 {
     $path = str_replace('AlibabaCloud\SDK\Ocrapi\V2021070\\', DIRECTORY_SEPARATOR, $class);
-    $file = __DIR__ . \DIRECTORY_SEPARATOR . 'src' . \DIRECTORY_SEPARATOR . $path . '.php';
+    $file = __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . $path . '.php';
     if (file_exists($file)) {
         require_once $file;
 
