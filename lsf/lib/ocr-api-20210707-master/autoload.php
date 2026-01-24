5975fa55422250ed46797d4fb5ffae2dfd0e113d
mengrui <mengrui@okay.cn>
2026-01-24 18:45:48 +0800

    test

--- lsf/lib/ocr-api-20210707-master/autoload.php
+++ lsf/lib/ocr-api-20210707-master/autoload.php
@@ -1,6 +1,6 @@
 <?php
 
-function classLoader($class)
+function ocrLoader($class)
 {
     $path = str_replace('AlibabaCloud\SDK\Ocrapi\V2021070\\', DIRECTORY_SEPARATOR, $class);
     $file = __DIR__ . \DIRECTORY_SEPARATOR . 'src' . \DIRECTORY_SEPARATOR . $path . '.php';
@@ -13,4 +13,4 @@ function classLoader($class)
     return false;
 }
 
-spl_autoload_register('classLoader');
+spl_autoload_register('ocrLoader');
